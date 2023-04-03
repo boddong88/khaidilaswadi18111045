e304abd70e95281805862313ef438755b1160f8d
boddong88 <129741375+boddong88@users.noreply.github.com>
2023-04-03 18:09:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// toggle class active
+const navbarnav = document.querySelector ('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarnav.classList.toggle('active');
+};
+
+
+//klik dimna saja untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarnav.contains(e.target)){
+        navbarnav.classList.remove('active');
+    }
+});
